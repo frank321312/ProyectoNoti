cd59e9121b7f69aaf0e8026832793b16d8e7d17c
frank321312 <hectorsacaca1123@gmail.com>
2024-02-08 13:06:46 -0300

    Primer commit

---
+++ expreso.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+const port = 8070;
+const emailConfig = JSON.parse(process.env.DATOS_PRIVADOS);
+console.log(emailConfig);
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Archivos_HTML/practicasFlex.html'))
+});
+
+app.listen(port, () => {
+    console.log(`Servidor ejecutandose en http://localhost:${port}/`);
+});
+
+// export MYSQL_CONNECTION_STRING=mysql://username:password@host:port/database
